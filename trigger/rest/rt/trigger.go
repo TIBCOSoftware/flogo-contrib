c5aa54efea2d77dd52c79036732441fee41ed785
Frank <fmartinez@tibco.com>
2016-04-10 20:28:41 -0400

    update rest trigger to use endpoints; add simple test

--- trigger/rest/rt/trigger.go
+++ trigger/rest/rt/trigger.go
@@ -5,16 +5,18 @@ import (
 	"net/http"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/ext/trigger"
-	"github.com/TIBCOSoftware/flogo-lib/core/process"
 	"github.com/TIBCOSoftware/flogo-lib/core/processinst"
 	"github.com/julienschmidt/httprouter"
 	"github.com/op/go-logging"
+	"strings"
+	"fmt"
+	"io"
 )
 
 // log is the default package logger
 var log = logging.MustGetLogger("rest-trigger")
 
-// todo: switch to use endpoint registration
+var validMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE"}
 
 // RestTrigger REST trigger struct
 type RestTrigger struct {
@@ -37,18 +39,40 @@ func (t *RestTrigger) Metadata() *trigger.Metadata {
 func (t *RestTrigger) Init(processStarter processinst.Starter, config *trigger.Config) {
 
 	router := httprouter.New()
-	router.OPTIONS("/process/start", handleOption)
-	router.POST("/process/start", t.StartProcess)
 
 	addr := ":" + config.Settings["port"]
-	t.server = NewServer(addr, router)
-
 	t.processStarter = processStarter
+
+	endpoints := config.Endpoints
+
+	for _, endpoint := range endpoints {
+
+		if endpointIsValid(endpoint) {
+			method := strings.ToUpper(endpoint.Settings["method"])
+			path := endpoint.Settings["path"]
+
+			log.Debugf("REST Trigger: Registering endpoint [%s: %s] for Process: %s", method, path, endpoint.ProcessURI)
+
+			router.OPTIONS(path, handleOption) // for CORS
+			router.Handle(method, path, StartProcessHandler(t, endpoint.ProcessURI))
+
+		} else {
+			panic(fmt.Sprintf("Invalid endpoint: %v", endpoint))
+		}
+	}
+
+	log.Debugf("REST Trigger: Configured on port %s", config.Settings["port"])
+	t.server = NewServer(addr, router)
 }
 
 // Start implements trigger.Trigger.Start
 func (t *RestTrigger) Start() {
-	t.server.Start()
+	err := t.server.Start()
+
+	if err != nil {
+		log.Errorf("REST Trigger: Error starting - %v", err)
+	}
+
 }
 
 // Stop implements trigger.Trigger.Start
@@ -72,38 +96,86 @@ type IDResponse struct {
 	ID string `json:"id"`
 }
 
-// StartProcess starts a new Process Instance (POST "/process/start").
-//
-// To post a start process, try this at a shell:
-// $ curl -H "Content-Type: application/json" -X POST -d '{"processUri":"base"}' http://localhost:8080/process/start
-func (t *RestTrigger) StartProcess(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func StartProcessHandler(rt *RestTrigger, processURI string) httprouter.Handle {
 
-	w.Header().Add("Access-Control-Allow-Origin", "*")
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
-	req := &StartRequest{}
-	err := json.NewDecoder(r.Body).Decode(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+		log.Debugf("REST Trigger: Recieved request")
+
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+
+		params := make(map[string]string)
+		for _, param := range ps {
+			params[param.Key] = param.Value
+		}
+
+		var content interface{}
+		err := json.NewDecoder(r.Body).Decode(&content)
+		if err != nil {
+			switch {
+			case err == io.EOF:
+				// empty body
+				//todo should endpoint say if content is expected?
+			case err != nil:
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+		}
+
+		//data := map[string]interface{}{
+		//	"params": params,
+		//	"content": content,
+		//}
+		//
+		////todo: implement reply handler?
+		//id := rt.processStarter.StartProcessInstance(processURI, data, nil, nil)
+
+		//todo: fix StartProcessInstance to use map[string]interface{} and remove this
+		paramsJSON,_ := json.Marshal(params)
+		contentJSON,_ := json.Marshal(content)
+
+		dataJSON := map[string]string{
+			"params": string(paramsJSON),
+			"content": string(contentJSON),
+		}
+
+		if log.IsEnabledFor(logging.DEBUG) {
+			log.Debugf("REST Trigger: Starting Process [%s] - data: %v", processURI, dataJSON)
+		}
+
+		id := rt.processStarter.StartProcessInstance(processURI, dataJSON, nil, nil)
+
+		// If we didn't find it, 404
+		//w.WriteHeader(http.StatusNotFound)
+
+		resp := &IDResponse{ID: id}
+
+		encoder := json.NewEncoder(w)
+		encoder.Encode(resp)
+
+		w.WriteHeader(http.StatusOK)
 	}
-
-	id := t.processStarter.StartProcessInstance(req.ProcessURI, req.Data, nil, nil)
-
-	// If we didn't find it, 404
-	//w.WriteHeader(http.StatusNotFound)
-
-	resp := &IDResponse{ID: id}
-
-	encoder := json.NewEncoder(w)
-	encoder.Encode(resp)
-
-	w.WriteHeader(http.StatusOK)
 }
 
-// StartRequest describes a request for starting a ProcessInstance
-type StartRequest struct {
-	ProcessURI  string               `json:"processUri"`
-	Data        map[string]string    `json:"data"`
-	Interceptor *process.Interceptor `json:"interceptor"`
-	Patch       *process.Patch       `json:"patch"`
+////////////////////////////////////////////////////////////////////////////////////////
+// Utils
+
+func endpointIsValid(endpoint *trigger.EndpointConfig) bool {
+
+	if !stringInList(strings.ToUpper(endpoint.Settings["method"]), validMethods) {
+		return false
+	}
+
+	//validate path
+
+	return true
+}
+
+func stringInList(str string, list []string) bool {
+	for _, value := range list {
+		if value == str {
+			return true
+		}
+	}
+	return false
 }
--- trigger/rest/rt/trigger_test.go
+++ trigger/rest/rt/trigger_test.go
@@ -4,14 +4,81 @@ import (
 	"testing"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/ext/trigger"
+	"github.com/TIBCOSoftware/flogo-lib/core/processinst"
+	"fmt"
+	"encoding/json"
+	"net/http"
 )
 
-func TestRegistered(t *testing.T) {
-	act := trigger.Get("rest")
+const testConfig string = `{
+  "name": "rest",
+  "settings": {
+    "port": "8091"
+  },
+  "endpoints": [
+    {
+      "processURI": "local://testProcess",
+      "settings": {
+        "method": "POST",
+        "path": "/device/:id/reset"
+      }
+    }
+  ]
+}
+`
 
-	if act == nil {
+type TestStarter struct {
+
+}
+
+// StartProcessInstance implements processinst.Starter.StartProcessInstance
+func (ts *TestStarter) StartProcessInstance(processURI string, startData map[string]string, replyHandler processinst.ReplyHandler, execOptions *processinst.ExecOptions) string {
+	fmt.Printf("Started Process with data: %v", startData);
+	return "dummyid"
+}
+
+func TestRegistered(t *testing.T) {
+	tgr := trigger.Get("rest")
+
+	if tgr == nil {
 		t.Error("Trigger Not Registered")
 		t.Fail()
 		return
+	}
+}
+
+func TestInit(t *testing.T) {
+	tgr := trigger.Get("rest")
+
+	starter := &TestStarter{}
+
+	config := &trigger.Config{}
+	json.Unmarshal([]byte(testConfig), config)
+
+	tgr.Init(starter, config)
+}
+
+func TestEndpoint(t *testing.T) {
+
+	tgr := trigger.Get("rest")
+
+	tgr.Start()
+	defer tgr.Stop()
+
+	uri := "http://127.0.0.1:8091/device/12345/reset"
+
+	req, err := http.NewRequest("POST", uri, nil)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	log.Debug("response Status:", resp.Status)
+
+	if resp.StatusCode >= 300 {
+		t.Fail()
 	}
 }
