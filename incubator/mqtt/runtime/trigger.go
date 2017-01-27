fba544cea964fcc8fb697d3fefc0288bc4b4c926
vnalawad <vnalawad@tibco.com>
2017-01-26 19:50:42 -0800

    MQTT Trigger

---
+++ incubator/mqtt/runtime/trigger.go
@@ -0,0 +1,203 @@
+package mqtt
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/TIBCOSoftware/flogo-lib/core/action"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+	"github.com/TIBCOSoftware/flogo-lib/flow/support"
+	"github.com/TIBCOSoftware/flogo-lib/types"
+	"github.com/eclipse/paho.mqtt.golang"
+	"github.com/op/go-logging"
+	"strconv"
+)
+
+const (
+	TRIGGER_REF = "github.com/TIBCOSoftware/flogo-contrib/incubator/mqtt/runtime"
+)
+
+// log is the default package logger
+var log = logging.MustGetLogger("trigger-tibco-mqtt")
+
+var md = trigger.NewMetadata(jsonMetadata)
+
+// todo: switch to use endpoint registration
+
+// MqttTrigger is simple MQTT trigger
+type MqttTrigger struct {
+	metadata          *trigger.Metadata
+	runner            action.Runner
+	client            mqtt.Client
+	settings          map[string]string
+	config            trigger.Config
+	topicToActionURI  map[string]string
+	topicToActionType map[string]string
+	myId              string
+}
+
+type MQTTFactory struct {
+}
+
+func (t *MQTTFactory) New(id string) trigger.Trigger2 {
+	return &MqttTrigger{metadata: md, myId: id}
+}
+func init() {
+
+	trigger.RegisterFactory(TRIGGER_REF, &MQTTFactory{})
+}
+
+// Metadata implements trigger.Trigger.Metadata
+func (t *MqttTrigger) Metadata() *trigger.Metadata {
+	return t.metadata
+}
+
+// Init implements ext.Trigger.Init
+func (t *MqttTrigger) Init(config types.TriggerConfig, runner action.Runner) {
+	var triggerConfig trigger.Config
+	err := json.Unmarshal(config.Data, &triggerConfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	t.config = triggerConfig
+	t.settings = triggerConfig.Settings
+	t.runner = runner
+}
+
+// Start implements ext.Trigger.Start
+func (t *MqttTrigger) Start() error {
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(t.settings["broker"])
+	opts.SetClientID(t.settings["id"])
+	opts.SetUsername(t.settings["user"])
+	opts.SetPassword(t.settings["password"])
+	b, err := strconv.ParseBool(t.settings["cleansess"])
+	if err != nil {
+		log.Error("Error converting \"cleansess\" to a boolean ", err.Error())
+		return err
+	}
+	opts.SetCleanSession(b)
+	if t.settings["store"] != ":memory:" {
+		opts.SetStore(mqtt.NewFileStore(t.settings["store"]))
+	}
+
+	opts.SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
+		topic := msg.Topic()
+		//TODO we should handle other types, since mqtt message format are data-agnostic
+		payload := string(msg.Payload())
+		log.Debug("Received msg:", payload)
+		actionURI, found := t.topicToActionURI[topic]
+		if found {
+			t.RunAction(actionURI, payload)
+		} else {
+			log.Errorf("Topic %s not found", t.topicToActionURI[topic])
+		}
+	})
+
+	client := mqtt.NewClient(opts)
+	t.client = client
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	i, err := strconv.Atoi(t.settings["qos"])
+	if err != nil {
+		log.Error("Error converting \"qos\" to an integer ", err.Error())
+		return err
+	}
+
+	t.topicToActionType = make(map[string]string)
+	t.topicToActionURI = make(map[string]string)
+
+	for _, endpoint := range t.config.Endpoints {
+		if token := t.client.Subscribe(endpoint.Settings["topic"], byte(i), nil); token.Wait() && token.Error() != nil {
+			log.Errorf("Error subscribing to topic %s: %s", endpoint.Settings["topic"], token.Error())
+			panic(token.Error())
+		} else {
+			t.topicToActionURI[endpoint.Settings["topic"]] = endpoint.ActionId
+		}
+	}
+
+	return nil
+}
+
+// Stop implements ext.Trigger.Stop
+func (t *MqttTrigger) Stop() error {
+	//unsubscribe from topic
+	log.Debug("Unsubcribing from topic: ", t.settings["topic"])
+	for _, endpoint := range t.config.Endpoints {
+		if token := t.client.Unsubscribe(endpoint.Settings["topic"]); token.Wait() && token.Error() != nil {
+			log.Errorf("Error unsubscribing from topic %s: %s", endpoint.Settings["topic"], token.Error())
+		}
+	}
+
+	t.client.Disconnect(250)
+
+	return nil
+}
+
+// RunAction starts a new Process Instance
+func (t *MqttTrigger) RunAction(actionURI string, payload string) {
+
+	req := t.constructStartRequest(payload)
+	//err := json.NewDecoder(strings.NewReader(payload)).Decode(req)
+	//if err != nil {
+	//	//http.Error(w, err.Error(), http.StatusBadRequest)
+	//	log.Error("Error Starting action ", err.Error())
+	//	return
+	//}
+
+	//todo handle error
+	startAttrs, _ := t.metadata.OutputsToAttrs(req.Data, false)
+
+	action := action.Get2(actionURI)
+	log.Infof("Found action: '%+x'", action)
+	log.Infof("ActionID: '%s'", actionURI)
+	context := trigger.NewContext(context.Background(), startAttrs)
+	_, replyData, err := t.runner.Run(context, action, actionURI, nil)
+	if err != nil {
+		log.Error("Error starting action: ", err.Error())
+	}
+	log.Debugf("Ran action: [%s]", actionURI)
+
+	if replyData != nil {
+		data, err := json.Marshal(replyData)
+		if err != nil {
+			log.Error(err)
+		} else {
+			t.publishMessage(req.ReplyTo, string(data))
+		}
+	}
+}
+
+func (t *MqttTrigger) publishMessage(topic string, message string) {
+
+	log.Debug("ReplyTo topic: ", topic)
+	log.Debug("Publishing message: ", message)
+
+	qos, err := strconv.Atoi(t.settings["qos"])
+	if err != nil {
+		log.Error("Error converting \"qos\" to an integer ", err.Error())
+		return
+	}
+	token := t.client.Publish(topic, byte(qos), false, message)
+	token.Wait()
+}
+
+func (t *MqttTrigger) constructStartRequest(message string) *StartRequest {
+	//TODO how to handle reply to, reply feature
+	req := &StartRequest{}
+	data := make(map[string]interface{})
+	data["message"] = message
+	req.Data = data
+	return req
+}
+
+// StartRequest describes a request for starting a ProcessInstance
+type StartRequest struct {
+	ProcessURI  string                 `json:"flowUri"`
+	Data        map[string]interface{} `json:"data"`
+	Interceptor *support.Interceptor   `json:"interceptor"`
+	Patch       *support.Patch         `json:"patch"`
+	ReplyTo     string                 `json:"replyTo"`
+}
---
+++ incubator/mqtt/runtime/trigger_test.go
@@ -0,0 +1,88 @@
+package mqtt
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/action"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+const testConfig string = `{
+  "name": "tibco-mqtt",
+  "settings": {
+    "topic": "flogo/#",
+    "broker": "tcp://192.168.1.12:1883",
+    "id": "flogoEngine",
+    "user": "",
+    "password": "",
+    "store": "",
+    "qos": "0",
+    "cleansess": "false"
+  },
+  "endpoints": [
+    {
+      "flowURI": "local://testFlow",
+      "settings": {
+        "topic": "test_start"
+      }
+    }
+  ]
+}`
+
+type TestRunner struct {
+}
+
+// Run implements action.Runner.Run
+func (tr *TestRunner) Run(context context.Context, action action.Action, uri string, options interface{}) (code int, data interface{}, err error) {
+	log.Debugf("Ran Action: %v", uri)
+	return 0, nil, nil
+}
+
+func TestRegistered(t *testing.T) {
+	act := trigger.Get("tibco-mqtt")
+
+	if act == nil {
+		t.Error("Trigger Not Registered")
+		t.Fail()
+		return
+	}
+}
+
+func TestInit(t *testing.T) {
+	tgr := trigger.Get("tibco-mqtt")
+
+	runner := &TestRunner{}
+
+	config := &trigger.Config{}
+	json.Unmarshal([]byte(testConfig), config)
+	tgr.Init(config, runner)
+}
+
+func TestEndpoint(t *testing.T) {
+
+	tgr := trigger.Get("tibco-mqtt")
+
+	tgr.Start()
+	defer tgr.Stop()
+
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker("tcp://192.168.1.12:1883")
+	opts.SetClientID("flogoEngine")
+	opts.SetUsername("")
+	opts.SetPassword("")
+	opts.SetCleanSession(false)
+
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	log.Debug("---- doing publish ----")
+	token := client.Publish("flogo/test_start", 0, false, "Test message payload!")
+	token.Wait()
+
+	client.Disconnect(250)
+	log.Debug("Sample Publisher Disconnected")
+}
