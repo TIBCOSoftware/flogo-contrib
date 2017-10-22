76881cd20c1327a90076fa46ad8bd154462672ba
teivah <harsanta@skysoft-atm.com>
2017-10-22 10:58:58 +0200

    Couchbase init

---
+++ activity/couchbase/activity_test.go
@@ -0,0 +1,69 @@
+package log
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("message", "test message")
+	tc.SetInput("flowInfo", true)
+
+	act.Eval(tc)
+}
+
+func TestAddToFlow(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput("message", "test message")
+	tc.SetInput("flowInfo", true)
+	tc.SetInput("addToFlow", true)
+
+	act.Eval(tc)
+
+	msg := tc.GetOutput("message")
+
+	fmt.Println("Message: ", msg)
+
+	if msg == nil {
+		t.Fail()
+	}
+}
