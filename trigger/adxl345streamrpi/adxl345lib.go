4d19a7b0de7face77eab34c20ce760ea2144d927
Philippe GABERT <pgabert@philippes-mbp.apac.tibco.com>
2018-02-12 16:22:17 +1100

    First DRAFT of ADXL345 trigger

---
+++ trigger/adxl345streamrpi/adxl345lib.go
@@ -0,0 +1,178 @@
+package adxl345streamrpi
+
+import (
+	"github.com/kidoman/embd"
+	"fmt"
+	"errors"
+	"encoding/binary"
+	"bytes"
+)
+
+const (
+	regDevid         = 0x00
+	regThreshTap     = 0x1d
+	regOfsX          = 0x1e
+	regOfsY          = 0x1f
+	regOfsZ          = 0x20
+	regDur           = 0x21
+	regLatent        = 0x22
+	regWindow        = 0x23
+	regThreshAct     = 0x24
+	regThreshInact   = 0x25
+	regTimeInact     = 0x26
+	regActInact_Ctl  = 0x27
+	regThreshFF      = 0x28
+	regTimeFF        = 0x29
+	regTapAxes       = 0x2a
+	regActTap_Status = 0x2b
+	regBWRate        = 0x2c
+	regPowerCtl      = 0x2d
+	regIntEnable     = 0x2e
+	regIntMap        = 0x2f
+	regIntSource     = 0x30
+	regDataFormat    = 0x31
+	regDataX0        = 0x32
+	regDataX1        = 0x33
+	regDataY0        = 0x34
+	regDataY1        = 0x35
+	regDataZ0        = 0x36
+	regDataZ1        = 0x37
+	regFifoCtl       = 0x38
+	regFifoStatus    = 0x39
+)
+
+const (
+	powerCtl8Hz       byte = 0x00
+	powerCtl4Hz       byte = 0x01
+	powerCtl2Hz       byte = 0x02
+	powerCtl1Hz       byte = 0x03
+	powerCtlSleep     byte = 0x04
+	powerCtlMeasure   byte = 0x08
+	powerCtlAutoSleep byte = 0x10
+	powerCtlLink      byte = 0x20
+)
+
+const (
+	dataFormatRange2g   byte = 0x00
+	dataFormatRange4g   byte = 0x01
+	dataFormatRange8g   byte = 0x02
+	dataFormatRange16g  byte = 0x03
+	dataFormatJustify   byte = 0x04
+	dataFormatFullRes   byte = 0x08
+	dataFormatIntInvert byte = 0x20
+	dataFormatSpi       byte = 0x40
+	dataFormatSelfTest  byte = 0x80
+)
+
+const (
+	bwRate6_25 byte = 0x06
+	bwRate12_5 byte = 0x07
+	bwRate25   byte = 0x08
+	bwRate50   byte = 0x09
+	bwRate100  byte = 0x0a
+	bwRate200  byte = 0x0b
+	bwRate400  byte = 0x0c
+	bwRate800  byte = 0x0d
+	bwRate1600 byte = 0x0e
+	bwRate3200 byte = 0x0f
+)
+
+
+const deviceID byte = 0xE5
+const DeviceAddr byte = 0x53
+const fullResolutionScaleFactor float32 = 3.9
+
+type Adxl345 struct {
+	Bus      embd.I2CBus
+	Opt      *Opt
+	device  int
+	address uint8
+}
+
+type Opt struct {
+
+}
+
+// NewOpt : initialize opts
+func NewOpt() *Opt {
+	return &Opt{
+		
+	}
+}
+
+type Acceleration struct {
+	data [3]float32 /* mg */
+}
+
+
+// New : initialize ADXL345
+func New(bus embd.I2CBus, opt *Opt) (*Adxl345, error) {
+	adxl := &Adxl345{Bus: bus, Opt: opt}
+	if err := adxl.setup(); err != nil {
+		return nil, err
+	}
+	return adxl, nil
+}
+
+func (adxl *Adxl345) setup() error {
+
+	if err := adxl.checkDevID(); err != nil {
+		fmt.Sprintf(err.Error())
+	}
+	adxl.Bus.WriteByteToReg(DeviceAddr, regDataFormat, dataFormatRange16g|dataFormatFullRes)
+	adxl.Bus.WriteByteToReg(DeviceAddr, regBWRate, bwRate400)
+	adxl.Bus.WriteByteToReg(DeviceAddr, regPowerCtl, powerCtlMeasure)
+	return nil
+
+}
+
+func (adxl *Adxl345) checkDevID() error {
+	data := []byte{0}
+
+	adxl.Bus.WriteByte(DeviceAddr, regDevid)
+	data, _ = adxl.Bus.ReadBytes(DeviceAddr, 0)
+
+	if data[0] != deviceID {
+		errors.New(fmt.Sprintf("ADXL345 at %x on bus %d returned wrong device id: %x\n", adxl.address, adxl.device, data[0]))
+	}
+
+	return nil
+}
+
+func (adxl *Adxl345) Destroy() {
+}
+
+
+func (adxl *Adxl345) Read() (*Acceleration, error) {
+	data := make([]byte, 6, 6)
+	ret := &Acceleration{}
+	var xReg int16
+	var yReg int16
+	var zReg int16
+
+	err := adxl.Bus.WriteByte(DeviceAddr, regDataX0)
+
+	if err != nil {
+		return ret, err
+	}
+	_, err = adxl.Bus.ReadByte(DeviceAddr)
+	if err != nil {
+		return ret, err
+	}
+
+	buf := bytes.NewBuffer(data)
+
+	binary.Read(buf, binary.LittleEndian, &xReg)
+	binary.Read(buf, binary.LittleEndian, &yReg)
+	binary.Read(buf, binary.LittleEndian, &zReg)
+	if err != nil {
+		return ret, err
+	}
+	
+	ret.data[0] = float32(xReg) * fullResolutionScaleFactor
+	ret.data[1] = float32(yReg) * fullResolutionScaleFactor
+	ret.data[2] = float32(zReg) * fullResolutionScaleFactor
+
+	return ret, nil
+}
+
---
+++ trigger/adxl345streamrpi/trigger.go
@@ -0,0 +1,146 @@
+package adxl345streamrpi
+
+import (
+	"context"
+	"github.com/TIBCOSoftware/flogo-lib/core/action"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi" // Mandatory for embd
+	"strconv"
+	"time"
+)
+
+// log is the default package logger
+var log = logger.GetLogger("trigger-adxsl345-rpi")
+
+var interval = 500
+
+// ADXL345Factory My Trigger factory
+type ADXL345Factory struct {
+	metadata *trigger.Metadata
+}
+
+//NewFactory create a new Trigger factory
+func NewFactory(md *trigger.Metadata) trigger.Factory {
+	return &ADXL345Factory{metadata: md}
+}
+
+//New Creates a new trigger instance for a given id
+func (t *ADXL345Factory) New(config *trigger.Config) trigger.Trigger {
+	return &ADXL345Trigger{metadata: t.metadata, config: config}
+}
+
+// ADXL345Trigger is a stub for your Trigger implementation
+type ADXL345Trigger struct {
+	metadata *trigger.Metadata
+	runner   action.Runner
+	config   *trigger.Config
+}
+
+func doEvery(d time.Duration, f func()) {
+	for _ = range time.Tick(d) {
+		f()
+	}
+}
+
+// Init implements trigger.Trigger.Init
+func (t *ADXL345Trigger) Init(runner action.Runner) {
+	t.runner = runner
+
+	if t.config.Settings == nil {
+		log.Infof("No configuration set for the trigger... Using default configuration...")
+	} else {
+		if t.config.Settings["delay_ms"] != nil && t.config.Settings["delay_ms"] != "" {
+			interval, _ = strconv.Atoi(t.config.GetSetting("delay_ms"))
+		} else {
+			log.Infof("No delay has been set. Using default value (", interval, "ms)")
+		}
+	}
+
+	log.Infof("In init, id: '%s', Metadata: '%+v', Config: '%+v'", t.config.Id, t.metadata, t.config)
+}
+
+// Metadata implements trigger.Trigger.Metadata
+func (t *ADXL345Trigger) Metadata() *trigger.Metadata {
+	return t.metadata
+}
+
+// Start implements trigger.Trigger.Start
+func (t *ADXL345Trigger) Start() error {
+	// start the trigger
+	log.Debug("Start Trigger ADXL345Stream for Raspberry PI")
+	handlers := t.config.Handlers
+	//t.timers = make(map[string]*scheduler.Job)
+
+	log.Debug("Processing handlers")
+	for _, handler := range handlers {
+		t.scheduleRepeating(handler)
+		log.Debugf("Processing Handler: %s", handler.ActionId)
+	}
+	return nil
+}
+
+// Stop implements trigger.Trigger.Start
+func (t *ADXL345Trigger) Stop() error {
+	// stop the trigger
+	return nil
+}
+
+func (t *ADXL345Trigger) scheduleRepeating(endpoint *trigger.HandlerConfig) {
+
+	log.Debug("Repeating every ", interval, "ms")
+
+	fn2 := func() {
+		act := action.Get(endpoint.ActionId)
+
+		x, y, z,  err := t.getDataFromSensor(endpoint)
+		if err != nil {
+			log.Error("Error while reading sensor data. Err: ", err.Error())
+		}
+
+		data := make(map[string]interface{})
+		data["x"] = x
+		data["y"] = y
+		data["z"] = z
+
+		log.Debug("Acceleration: [x=", data["x"], " , y=", data["y"], ", z=", data["z"], "]")
+		startAttrs, err := t.metadata.OutputsToAttrs(data, true)
+
+		if err != nil {
+			log.Errorf("After run error' %s'\n", err)
+		}
+
+		ctx := trigger.NewContext(context.Background(), startAttrs)
+		results, err := t.runner.RunAction(ctx, act, nil)
+
+		if err != nil {
+			log.Errorf("An error occured while starting the flow. Err:", err)
+		}
+		log.Info("Exec: ", results)
+	}
+
+	// schedule repeating
+	doEvery(time.Duration(interval)*time.Millisecond, fn2)
+}
+
+func (t *ADXL345Trigger) getDataFromSensor(endpoint *trigger.HandlerConfig) (x float32, y float32, z float32, err error) {
+
+	opt := NewOpt()
+	adxl345, errNew := New(embd.NewI2CBus(1), opt)
+
+	if errNew != nil {
+		log.Errorf("Error while creating the sensor reader !' %s'\n", errNew)
+		err = errNew
+	}
+
+	accel, errRead := adxl345.Read()
+	if errRead != nil {
+		log.Errorf("Error while reading the data !!' %s'\n", errRead)
+		err = errRead
+	}
+	x = accel.data[0]
+	y = accel.data[1]
+	z = accel.data[2]
+	return x, y, z, err
+}
