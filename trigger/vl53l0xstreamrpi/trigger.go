5e5852f09d0459a494134e3892ed4a2f1bcbdaad
Philippe GABERT <pgabert@philippes-mbp.apac.tibco.com>
2018-02-12 11:03:58 +1100

    Mode back to back

--- trigger/vl53l0xstreamrpi/trigger.go
+++ trigger/vl53l0xstreamrpi/trigger.go
@@ -114,7 +114,7 @@ func NewDriver(bus embd.I2CBus) *VL530LXDriver {
 		bus:     bus,
 		address: VL53L0XAddress,
 	}
-	//d.bus.WriteByteToReg(d.address, 0x00, 0x01)
+	d.bus.WriteByteToReg(d.address, 0x00, 0x02)
 	
 	return d
 }
@@ -130,7 +130,7 @@ func (d *VL530LXDriver) Measure() (distance int , err error) {
 	if err != nil {
 		log.Error(err)
 	}
-	d.bus.WriteByteToReg(d.address, 0x02, 0x01)
+	
 	distance = int(binary.BigEndian.Uint16([]byte{byteA, byteB}))
 
 	return distance, err
