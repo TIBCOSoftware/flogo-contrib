4aaf042afdee1061f0b9d566845162f082c8bcb6
Matt Ellis <mellis@Matts-MacBook-Pro-3.local>
2018-04-27 09:14:53 -0600

    setting the correct resppnse object for api gw requests on lambda trigger

--- trigger/lambda/shim/shim.go
+++ trigger/lambda/shim/shim.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 
 	fl "github.com/TIBCOSoftware/flogo-contrib/trigger/lambda"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/lambdacontext"
 )
@@ -16,11 +17,62 @@ func Handle(ctx context.Context, evt json.RawMessage) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Get the actual lambda event object. Right now just using the evt typ to return the correct response.
+	// This does not impact the event in the action (flow)
+	evtTyp, _ := getEvtType(evt)
+
+	// Invoke the flogo lambda trigger and handle the event
 	result, err := fl.Invoke()
 	if err != nil {
 		return nil, err
 	}
-	return result, nil
+
+	return coherceResponseObj(result, evtTyp), nil
+}
+
+func getEvtType(raw json.RawMessage) (string, interface{}) {
+	var evt map[string]interface{}
+	json.Unmarshal(raw, &evt)
+
+	if _, ok := evt["RequestContext"]; ok {
+		apiGw := events.ApiGatewayProxyRequest{}
+		json.Unmarshal(raw, &apiGw)
+		return "apigw", apiGw
+	}
+
+	return "", nil
+}
+
+func coherceResponseObj(result map[string]interface{}, evtTyp string) interface{} {
+	var returnObj interface{}
+
+	responseData := result["data"]
+	statusCode := result["status"].(int)
+
+	// Marshal the response
+	responseRaw, _ := json.Marshal(responseData)
+
+	// Check if API GW request. If so, build the correct response
+	switch evtTyp {
+	case "apigw":
+		returnObj = events.ApiGatewayProxyResponse{
+			StatusCode: func() int {
+				if statusCode == 0 {
+					return 200
+				} else {
+					return statusCode
+				}
+			}(),
+			Body:            string(responseRaw),
+			IsBase64Encoded: false,
+		}
+
+	default:
+		returnObj = responseData
+	}
+
+	return returnObj
 }
 
 func setupArgs(evt json.RawMessage, ctx *context.Context) error {
