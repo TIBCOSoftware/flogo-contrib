471db71946c5bab83bf5a970ba9b2f99dc4f851f
Matt Ellis <mellis@tibco.com>
2018-05-03 17:35:55 -0600

    Lambda trigger updates (#246)
    * setting the correct resppnse object for api gw requests on lambda trigger
    
    * handling marshal error
    
    * fixing minor issues
    
    * fixing spelling mistake
    
    * using consts for event type rather than str
    
    * catching unmarshal error on evt check

--- trigger/lambda/shim/shim.go
+++ trigger/lambda/shim/shim.go
@@ -6,8 +6,13 @@ import (
 	"flag"
 
 	fl "github.com/TIBCOSoftware/flogo-contrib/trigger/lambda"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/lambdacontext"
+)
+
+const (
+	APIGW uint = 1 + iota
 )
 
 // Handle implements the Flogo Function handler
@@ -16,11 +21,67 @@ func Handle(ctx context.Context, evt json.RawMessage) (interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Get the actual lambda event object. Right now just using the evt typ to return the correct response.
+	// This does not impact the event in the action (flow)
+	evtTyp, _ := getEvtType(evt)
+
+	// Invoke the flogo lambda trigger and handle the event
 	result, err := fl.Invoke()
 	if err != nil {
 		return nil, err
 	}
-	return result, nil
+
+	return coerceResponseObj(result, evtTyp)
+}
+
+func getEvtType(raw json.RawMessage) (uint, interface{}) {
+	var evt map[string]interface{}
+	if err := json.Unmarshal(raw, &evt); err != nil {
+		return 0, nil
+	}
+
+	if _, ok := evt["requestContext"]; ok {
+		apiGw := events.APIGatewayProxyRequest{}
+		json.Unmarshal(raw, &apiGw)
+		return APIGW, apiGw
+	}
+
+	return 0, nil
+}
+
+func coerceResponseObj(result map[string]interface{}, evtTyp uint) (interface{}, error) {
+	var returnObj interface{}
+
+	responseData := result["data"]
+	statusCode := result["status"].(int)
+
+	// Marshal the response
+	responseRaw, err := json.Marshal(responseData)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if API GW request. If so, build the correct response
+	switch evtTyp {
+	case APIGW:
+		returnObj = events.APIGatewayProxyResponse{
+			StatusCode: func() int {
+				if statusCode == 0 {
+					return 200
+				} else {
+					return statusCode
+				}
+			}(),
+			Body:            string(responseRaw),
+			IsBase64Encoded: false,
+		}
+
+	default:
+		returnObj = responseData
+	}
+
+	return returnObj, nil
 }
 
 func setupArgs(evt json.RawMessage, ctx *context.Context) error {
