9ba82ba992c87da98ff6b8290bcd269b6aeaf2d4
Frank Martinez <fmartinez@tibco.com>
2018-08-15 15:04:28 -0400

    add accumulator function to additional windows (#282)

--- activity/aggregate/window/functions/accumulate.go
+++ activity/aggregate/window/functions/accumulate.go
@@ -1,5 +1,22 @@
 package functions
 
+
+func AddSampleAccum(a, b interface{}) interface{} {
+
+	var accum []interface{}
+
+	if a == nil {
+		accum = make([]interface{}, 0)
+	} else {
+		accum = a.([]interface{})
+	}
+
+	accum = append(accum, b)
+
+	return accum
+}
+
+
 func AggregateBlocksAccumulate(blocks []interface{}, start int, size int) interface{} {
 
 	accum := make([]interface{}, 0, len(blocks))
