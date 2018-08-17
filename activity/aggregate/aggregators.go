9701ef4876daf1ccdf45f2dccf38fc6ce7186fad
Frank Martinez <fmartinez@tibco.com>
2018-08-17 08:16:39 -0400

    fix aggregate window internal settings (#287)
    * fix aggregate window settings
    
    * fix aggregate activity.json

--- activity/aggregate/aggregators.go
+++ activity/aggregate/aggregators.go
@@ -7,20 +7,20 @@ import (
 	"github.com/TIBCOSoftware/flogo-contrib/activity/aggregate/window/functions"
 )
 
-func NewTumblingWindow(function string, windowSize int) (window.Window, error) {
+func NewTumblingWindow(function string, settings *window.Settings) (window.Window, error) {
 	switch function {
 	case "avg":
-		return window.NewTumblingWindow(functions.AddSampleSum, functions.AggregateSingleAvg, windowSize), nil
+		return window.NewTumblingWindow(functions.AddSampleSum, functions.AggregateSingleAvg, settings), nil
 	case "sum":
-		return window.NewTumblingWindow(functions.AddSampleSum, functions.AggregateSingleNoopFunc, windowSize), nil
+		return window.NewTumblingWindow(functions.AddSampleSum, functions.AggregateSingleNoopFunc, settings), nil
 	case "min":
-		return window.NewTumblingWindow(functions.AddSampleMin, functions.AggregateSingleNoopFunc, windowSize), nil
+		return window.NewTumblingWindow(functions.AddSampleMin, functions.AggregateSingleNoopFunc, settings), nil
 	case "max":
-		return window.NewTumblingWindow(functions.AddSampleMax, functions.AggregateSingleNoopFunc, windowSize), nil
+		return window.NewTumblingWindow(functions.AddSampleMax, functions.AggregateSingleNoopFunc, settings), nil
 	case "count":
-		return window.NewTumblingWindow(functions.AddSampleCount, functions.AggregateSingleNoopFunc, windowSize), nil
+		return window.NewTumblingWindow(functions.AddSampleCount, functions.AggregateSingleNoopFunc, settings), nil
 	case "accumulate":
-		return window.NewTumblingWindow(functions.AddSampleAccum, functions.AggregateSingleNoopFunc, windowSize), nil
+		return window.NewTumblingWindow(functions.AddSampleAccum, functions.AggregateSingleNoopFunc, settings), nil
 	default:
 		return nil, fmt.Errorf("unsupported function: %s", function)
 	}
@@ -28,39 +28,39 @@ func NewTumblingWindow(function string, windowSize int) (window.Window, error) {
 
 // NewTumblingTimeWindow creates a new tumbling time window, all time windows are managed
 // externally and are progressed using the NextBlock() method
-func NewTumblingTimeWindow(function string, windowTime int, externalTimer bool) (window.TimeWindow, error) {
+func NewTumblingTimeWindow(function string, settings *window.Settings) (window.TimeWindow, error) {
 	switch function {
 	case "avg":
-		return window.NewTumblingTimeWindow(functions.AddSampleSum, functions.AggregateSingleAvg, windowTime, externalTimer), nil
+		return window.NewTumblingTimeWindow(functions.AddSampleSum, functions.AggregateSingleAvg, settings), nil
 	case "sum":
-		return window.NewTumblingTimeWindow(functions.AddSampleSum, functions.AggregateSingleNoopFunc, windowTime, externalTimer), nil
+		return window.NewTumblingTimeWindow(functions.AddSampleSum, functions.AggregateSingleNoopFunc, settings), nil
 	case "min":
-		return window.NewTumblingTimeWindow(functions.AddSampleMin, functions.AggregateSingleNoopFunc, windowTime, externalTimer), nil
+		return window.NewTumblingTimeWindow(functions.AddSampleMin, functions.AggregateSingleNoopFunc, settings), nil
 	case "max":
-		return window.NewTumblingTimeWindow(functions.AddSampleMax, functions.AggregateSingleNoopFunc, windowTime, externalTimer), nil
+		return window.NewTumblingTimeWindow(functions.AddSampleMax, functions.AggregateSingleNoopFunc, settings), nil
 	case "count":
-		return window.NewTumblingTimeWindow(functions.AddSampleCount, functions.AggregateSingleNoopFunc, windowTime, externalTimer), nil
+		return window.NewTumblingTimeWindow(functions.AddSampleCount, functions.AggregateSingleNoopFunc, settings), nil
 	case "accumulate":
-		return window.NewTumblingTimeWindow(functions.AddSampleAccum, functions.AggregateSingleNoopFunc, windowTime, externalTimer), nil
+		return window.NewTumblingTimeWindow(functions.AddSampleAccum, functions.AggregateSingleNoopFunc, settings), nil
 	default:
 		return nil, fmt.Errorf("unsupported function: %s", function)
 	}
 }
 
-func NewSlidingWindow(function string, windowSize int) (window.Window, error) {
+func NewSlidingWindow(function string, settings *window.Settings) (window.Window, error) {
 	switch function {
 	case "avg":
-		return window.NewSlidingWindow(functions.AggregateBlocksAvg, windowSize, 1), nil
+		return window.NewSlidingWindow(functions.AggregateBlocksAvg, settings), nil
 	case "sum":
-		return window.NewSlidingWindow(functions.AggregateBlocksSum, windowSize, 1), nil
+		return window.NewSlidingWindow(functions.AggregateBlocksSum, settings), nil
 	case "min":
-		return window.NewSlidingWindow(functions.AggregateBlocksMin, windowSize, 1), nil
+		return window.NewSlidingWindow(functions.AggregateBlocksMin, settings), nil
 	case "max":
-		return window.NewSlidingWindow(functions.AggregateBlocksMax, windowSize, 1), nil
+		return window.NewSlidingWindow(functions.AggregateBlocksMax, settings), nil
 	case "count":
-		return window.NewSlidingWindow(functions.AggregateBlocksCount, windowSize, 1), nil
+		return window.NewSlidingWindow(functions.AggregateBlocksCount, settings), nil
 	case "accumulate":
-		return window.NewSlidingWindow(functions.AggregateBlocksAccumulate, windowSize, 1), nil
+		return window.NewSlidingWindow(functions.AggregateBlocksAccumulate, settings), nil
 	default:
 		return nil, fmt.Errorf("unsupported function: %s", function)
 	}
@@ -68,18 +68,18 @@ func NewSlidingWindow(function string, windowSize int) (window.Window, error) {
 
 // NewSlidingTimeWindow creates a new sliding time window, all time windows are managed
 // externally and are progressed using the NextBlock() method
-func NewSlidingTimeWindow(function string, windowTime int, resolution int, externalTimer bool) (window.TimeWindow, error) {
+func NewSlidingTimeWindow(function string, settings *window.Settings) (window.TimeWindow, error) {
 	switch function {
 	case "avg":
-		return window.NewSlidingTimeWindow(functions.AddSampleSum, functions.AggregateBlocksAvg, windowTime, resolution, externalTimer), nil
+		return window.NewSlidingTimeWindow(functions.AddSampleSum, functions.AggregateBlocksAvg, settings), nil
 	case "sum":
-		return window.NewSlidingTimeWindow(functions.AddSampleSum, functions.AggregateBlocksSum, windowTime, resolution, externalTimer), nil
+		return window.NewSlidingTimeWindow(functions.AddSampleSum, functions.AggregateBlocksSum, settings), nil
 	case "min":
-		return window.NewSlidingTimeWindow(functions.AddSampleMin, functions.AggregateBlocksMin, windowTime, resolution, externalTimer), nil
+		return window.NewSlidingTimeWindow(functions.AddSampleMin, functions.AggregateBlocksMin, settings), nil
 	case "max":
-		return window.NewSlidingTimeWindow(functions.AddSampleMax, functions.AggregateBlocksMax, windowTime, resolution, externalTimer), nil
+		return window.NewSlidingTimeWindow(functions.AddSampleMax, functions.AggregateBlocksMax, settings), nil
 	case "count":
-		return window.NewSlidingTimeWindow(functions.AddSampleCount, functions.AggregateBlocksSum, windowTime, resolution, externalTimer), nil
+		return window.NewSlidingTimeWindow(functions.AddSampleCount, functions.AggregateBlocksSum, settings), nil
 	default:
 		return nil, fmt.Errorf("unsupported function: %s", function)
 	}
