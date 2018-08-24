cadbe9b943dbe20318c8d2ec3d986e79a0f80faa
Abram Van Der Geest <avanderg@tibco.com>
2018-08-24 14:52:40 -0400

    the inputs error had already been fixed, I just undid it.  So I fixed that error

--- activity/inference/activity_test.go
+++ activity/inference/activity_test.go
@@ -54,29 +54,45 @@ func TestEval(t *testing.T) {
 	tc := test.NewTestActivityContext(getActivityMetadata())
 
 	//setup attrs
-	tc.SetInput("model", "/Users/mellis/Documents/IoT/models/tn_demo/Archive.zip")
-	tc.SetInput("inputName", "input")
+	tc.SetInput("model", "/Users/avanders/working/working_python/accelerometer/models/TB/1531761580")
+	tc.SetInput("inputName", "inputs")
 	tc.SetInput("framework", "Tensorflow")
 
 	var features = make(map[string]interface{})
-	features["z-axis-q75"] = 4.140586
-	features["corr-x-z"] = 0.1381063882214782
-	features["x-axis-mean"] = 1.7554575428900194
-	features["z-axis-sd"] = 4.6888631696380765
-	features["z-axis-skew"] = -0.3619011587545954
-	features["y-axis-sd"] = -7.959084724314854
-	features["y-axis-q75"] = 16.467001
-	features["corr-z-y"] = 0.3467060369518231
-	features["x-axis-sd"] = 6.450293741961166
-	features["x-axis-skew"] = 0.09756801680727022
-	features["y-axis-mean"] = 9.389463650669393
-	features["y-axis-skew"] = -0.49036224958471764
-	features["z-axis-mean"] = 1.1226106985139188
-	features["x-axis-q25"] = -3.1463003
-	features["x-axis-q75"] = 6.3198414
-	features["y-axis-q25"] = 3.0645783
-	features["z-axis-q25"] = -1.9477097
-	features["corr-x-y"] = 0.08100326860866637
+	features["x"] = -1.961143
+	features["y"] = 2.371200
+	features["z"] = 9.746658
+	features["x1"] = -1.859383
+	features["y1"] = 2.881411
+	features["z1"] = 9.595146
+	features["x2"] = -1.892647
+	features["y2"] = 3.019059
+	features["z2"] = 9.418503
+	features["x3"] = -2.028000
+	features["y3"] = 2.666912
+	features["z3"] = 8.900028
+	features["x4"] = -2.278714
+	features["y4"] = 4.023686
+	features["z4"] = 8.547684
+	features["x5"] = -2.377852
+	features["y5"] = 4.164970
+	features["z5"] = 8.326502
+	features["x6"] = -2.422589
+	features["y6"] = 4.343912
+	features["z6"] = 8.241617
+	features["x7"] = -2.496000
+	features["y7"] = 4.807030
+	features["z7"] = 7.697485
+	features["x8"] = -2.586618
+	features["y8"] = 4.956441
+	features["z8"] = 7.407708
+	features["x9"] = -3.515735
+	features["y9"] = 4.490735
+	features["z9"] = 7.653178
+	features["x10"] = -4.226486
+	features["y10"] = 4.313400
+	features["z10"] = 8.797283
+	features["activity"] = 1
 
 	tc.SetInput("features", features)
 
